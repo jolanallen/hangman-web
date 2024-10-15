232217f31ee99a618653e31d4fb02396dfd9b3d8
ajolan <jolan.allen@ynov.com>
2024-10-15 18:37:07 +0200

    struturation des fichier

--- server/main.go
+++ server/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-	"hangmanweb"
+	hangmanweb "hangmanweb/back/serveur"
 	"log"
 	"net/http"
-	//"html/template"
-	
 )
 
  func main() {
-	http.HandleFunc("/Play", hangmanweb.PLAY)
+	http.HandleFunc("/Play", hangmanweb.Play)
 	http.HandleFunc("/Home", hangmanweb.Home)
 	fmt.Println("Serveur démarré sur http://localhost:8080/Home")
 	log.Fatal(http.ListenAndServe(":8080", nil))
