7f1802c5c055a53dbd74c16474ed6328ffe23de3
ajolan <jolan.allen@ynov.com>
2024-10-15 16:09:46 +0200

    fichier

--- server/main.go
+++ server/main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"fmt"
-	//hw "hangmanweb/Hangman-web"
+	
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HOME\n")
-}
-func Jeu(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "JEU")
-}
+
 
 
  func main() {
-	http.HandleFunc("/Home", Home)
-	http.HandleFunc("/Jeu", Jeu)
+
 	http.ListenAndServe(":8080", nil)
  }
