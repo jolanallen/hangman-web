429284f35c3a621b676cd6c5c4963b44cdf36c01
ajolan <jolan.allen@ynov.com>
2024-11-08 15:07:54 +0100

    etat

--- back/serveur/server.go
+++ back/serveur/server.go
@@ -1,43 +1,42 @@
 package hangmanweb
 
 import (
-	//"fmt"
+	
 	"html/template"
 	"net/http"
 )
 
-var hc *hangman.HANGMAN
-var Donnees = Data{
-	Essai: 10,
-	//Essai: hc.Erreur,
-	//Mot: hc.Mot,
+
+
+var Data = Donnees{
+	Mot: "hc.Mot",
+	Find: "C _ I _ _",
+	
+	
 
 }
 
+var templateHome = "./web/templates/Home.html"
+var templatePlay = "./web/templates/Play.html"
+var templateWin = "./web/templates/Win.html"
+var templateLoose = "./web/templates/Loose.html"
+
 func (h *HANGMANWEB) Home(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(templateHome))
-	defer
 	tmpl.Execute(w, Data)
 	
 }
 
 func (h *HANGMANWEB) Play(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(templatePlay))
-	defer
 	tmpl.Execute(w, Data)
 }
 
 func (h *HANGMANWEB) Win(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(templateWin))
-	defer
 	tmpl.Execute(w, Data)
 }
 func (h *HANGMANWEB) Loose(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(templateLoose))
-	defer
 	tmpl.Execute(w, Data)
 }
-func (h *HANGMANWEB) Imput(w http.ResponseWriter, r *http.Request) {
-	
-}
-
--- back/serveur/structweb.go
+++ back/serveur/structweb.go
@@ -4,17 +4,19 @@ package hangmanweb
 
 
 
+
+type Donnees struct {
 type Donnees struct {
 	Essai   int
 	Mot     string
 	Find string
-	Usedletter string
-	
+
 }
 
 
 type HANGMANWEB struct {
 	
+
 
 
 }
